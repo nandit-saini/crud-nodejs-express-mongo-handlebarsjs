01c2b9c1b772cc36db4ddd0b73bd1e9579c6385b
Successive <successive@localhost.localdomain>
2016-11-21 19:36:24 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+
+
+var bodyParser = require('body-parser');
+var db = require('./config/db');
+
+app.use(express.static("public"));
+
+server.listen("8000");
+
+
+
+app.get("/",function(req,res){
+
+	res.sendFile(__dirname+"public/HomePage.html");
+
+
+});
